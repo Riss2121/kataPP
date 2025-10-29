8de46b460832d3bff3316bf9ead265c34d898d98
Riss2121 <gamerroma123456@gmail.com>
2025-10-29 21:34:56 +0300

    Zarabotalo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("!");
-    }
+        System.out.println("wsdasd");
+}
 }
